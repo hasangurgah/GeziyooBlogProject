248fb70ce20daf742607e33847956f517aeba7ad
Hasan Gürgah <hasangurgah@hotmail.com>
2023-06-01 21:51:55 +0300

    Add project files.

---
+++ TravelTripProject/Controllers/BlogController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TravelTripProject.Models.Siniflar;
+
+namespace TravelTripProject.Controllers
+{
+    public class BlogController : Controller
+    {
+        // GET: Blog
+        Context c = new Context();
+        public ActionResult BlogIndex()
+        {
+            var bloglar = c.Blogs.ToList();
+            return View(bloglar);
+        }
+        BlogYorum by = new BlogYorum();
+        public ActionResult BlogDetay(int id)
+        {
+            //var blogbul = c.Blogs.Where(x=>x.ID== id).ToList();
+            by.Deger1 = c.Blogs.Where(x => x.ID == id).ToList();
+            by.Deger2 = c.Yorumlars.Where(x => x.Blogid == id).ToList();
+            return View(by);
+        }
+    }
+}
